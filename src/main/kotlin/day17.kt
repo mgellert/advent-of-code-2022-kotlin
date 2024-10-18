f382581a216dbfe0ec18bd5648e56db27773b468
gellertm <mgellert@users.noreply.github.com>
2024-10-18 14:11:14 +0200

    Refactored day 17 a bit.

--- src/main/kotlin/day17.kt
+++ src/main/kotlin/day17.kt
@@ -1,12 +1,14 @@
-class PyroclasticFlow(private val jetPattern: String) {
+import java.io.File
 
-    fun heightOfRocks(): Int {
-        val moveItr = moveGenerator().iterator()
-        var shapeCount = 0
+object PyroclasticFlow : Solution {
+
+    fun heightOfRocks(jetPattern: String, maxShapes: Long): Long {
+        val moveItr = moveGenerator(jetPattern).iterator()
+        var shapeCounter = 0L
         val resting = mutableSetOf<Point>()
 
-        while (shapeCount < 2022) {
-            var rock = nextShape(shapeCount++, Point(2, 4 + (resting.maxOfOrNull { it.y } ?: -1)))
+        while (shapeCounter < maxShapes) {
+            var rock = nextShape(shapeCounter++, Point(2, 4 + (resting.maxOfOrNull { it.y } ?: 0)))
 
             while (true) {
                 val move = moveItr.next()
@@ -17,18 +19,16 @@ class PyroclasticFlow(private val jetPattern: String) {
                 }
                 val (hasFallen, r) = fall(rock, resting)
                 rock = r
-                if (!hasFallen) {
-                    break
-                }
+                if (!hasFallen) break
             }
 
             resting.addAll(rock)
         }
 
-        return resting.maxOf { it.y } + 1
+        return resting.maxOf { it.y }
     }
 
-    private fun moveGenerator() = sequence {
+    private fun moveGenerator(jetPattern: String) = sequence {
         var i = 0
         while (true) {
             yield(jetPattern[i++])
@@ -38,16 +38,16 @@ class PyroclasticFlow(private val jetPattern: String) {
         }
     }
 
-    private fun nextShape(n: Int, start: Point): Set<Point> {
+    private fun nextShape(n: Long, start: Point): Set<Point> {
         when (n % 5) {
-            0 -> return setOf(
+            0L -> return setOf(
                 start,
                 start.copy(x = start.x + 1),
                 start.copy(x = start.x + 2),
                 start.copy(x = start.x + 3),
             )
 
-            1 -> return setOf(
+            1L -> return setOf(
                 start.copy(x = start.x + 0, y = start.y + 1),
                 start.copy(x = start.x + 1, y = start.y + 0),
                 start.copy(x = start.x + 1, y = start.y + 1),
@@ -55,7 +55,7 @@ class PyroclasticFlow(private val jetPattern: String) {
                 start.copy(x = start.x + 2, y = start.y + 1),
             )
 
-            2 -> return setOf(
+            2L -> return setOf(
                 start.copy(x = start.x + 0, y = start.y + 0),
                 start.copy(x = start.x + 1, y = start.y + 0),
                 start.copy(x = start.x + 2, y = start.y + 0),
@@ -63,14 +63,14 @@ class PyroclasticFlow(private val jetPattern: String) {
                 start.copy(x = start.x + 2, y = start.y + 2),
             )
 
-            3 -> return setOf(
+            3L -> return setOf(
                 start.copy(x = start.x, y = start.y + 0),
                 start.copy(x = start.x, y = start.y + 1),
                 start.copy(x = start.x, y = start.y + 2),
                 start.copy(x = start.x, y = start.y + 3),
             )
 
-            4 -> return setOf(
+            4L -> return setOf(
                 start.copy(x = start.x + 0, y = start.y + 0),
                 start.copy(x = start.x + 1, y = start.y + 0),
                 start.copy(x = start.x + 0, y = start.y + 1),
@@ -101,12 +101,14 @@ class PyroclasticFlow(private val jetPattern: String) {
 
     private fun fall(rock: Set<Point>, resting: Set<Point>): Pair<Boolean, Set<Point>> {
         val movedRock = rock.map { it.copy(y = it.y - 1) }.toSet()
-        return if (movedRock.all { it.y >= 0 && !resting.contains(it) }) {
+        return if (movedRock.all { it.y > 0 && !resting.contains(it) }) {
             Pair(true, movedRock)
         } else {
             Pair(false, rock)
         }
     }
+
+    fun jetPatterns() = File("src/main/resources/inputs/day17").readText().trim()
 }
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Long, val y: Long)
