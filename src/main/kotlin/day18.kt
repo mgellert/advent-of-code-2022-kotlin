7993a0328972089beb9e6f7b51f0d1ea5c3fe47e
gellertm <mgellert@users.noreply.github.com>
2024-10-19 09:12:04 +0200

    Solved day 18 part 2.

--- src/main/kotlin/day18.kt
+++ src/main/kotlin/day18.kt
@@ -1,6 +1,9 @@
+import java.util.LinkedList
+import java.util.Queue
+
 object BoilingBoulders : Solution {
 
-    val deltas = listOf(
+    private val deltas = listOf(
         Point3D(-1, 0, 0),
         Point3D(1, 0, 0),
         Point3D(0, -1, 0),
@@ -10,11 +13,47 @@ object BoilingBoulders : Solution {
     )
 
     fun surfaceArea(droplets: Set<Point3D>): Int {
-        return droplets.map { droplet ->
+        return droplets.sumOf { droplet ->
             deltas
                 .map { d -> Point3D(droplet.x + d.x, droplet.y + d.y, droplet.z + d.z) }
                 .count { !droplets.contains(it) }
-        }.sum()
+        }
+    }
+
+    fun externalSurfaceArea(droplets: Set<Point3D>): Int {
+        // random point
+        val startPoint = Point3D(1, 1, 1)
+
+        val minX = droplets.minOfOrNull { it.x }!! - 1
+        val maxX = droplets.maxOfOrNull { it.x }!! + 1
+
+        val minY = droplets.minOfOrNull { it.y }!! - 1
+        val maxY = droplets.maxOfOrNull { it.y }!! + 1
+
+        val minZ = droplets.minOfOrNull { it.z }!! - 1
+        val maxZ = droplets.maxOfOrNull { it.z }!! + 1
+
+        val water = mutableSetOf<Point3D>()
+        val queue: Queue<Point3D> = LinkedList()
+        queue.add(startPoint)
+
+        while (queue.isNotEmpty()) {
+            val droplet = queue.poll()
+            deltas
+                .map { d -> Point3D(droplet.x + d.x, droplet.y + d.y, droplet.z + d.z) }
+                .filter { it.x in minX..maxX && it.y in minY..maxY && it.z in minZ..maxZ }
+                .filter { !droplets.contains(it) && !water.contains(it) }
+                .forEach {
+                    queue.add(it)
+                    water.add(it)
+                }
+        }
+
+        return droplets.sumOf { droplet ->
+            deltas
+                .map { d -> Point3D(droplet.x + d.x, droplet.y + d.y, droplet.z + d.z) }
+                .count { !droplets.contains(it) && water.contains(it) }
+        }
     }
 
     fun readDroplets() = readInput("day18")
