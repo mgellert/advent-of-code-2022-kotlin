5b6f2a6827fe4506d403ecd232c60b8259c42ba1
gellertm <mgellert@users.noreply.github.com>
2024-10-21 19:58:37 +0200

    Solved day 19 part 1.

---
+++ src/test/kotlin/Day19Test.kt
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day19Test {
+
+    @Test
+    fun `should parse a line of input`() {
+        val blueprint = NotEnoughMinerals.parseLine(testInput.split("\n").first())
+        assertEquals(
+            Blueprint(
+                1, Minerals(4, 0, 0), Minerals(2, 0, 0), Minerals(3, 14, 0), Minerals(2, 0, 7)
+            ), blueprint
+        )
+    }
+
+    @Test
+    fun `should find max geodes for blueprint 1 from test input`() {
+        val blueprint = NotEnoughMinerals.parseLine(testInput.split("\n").drop(1).first())
+
+        val quality = NotEnoughMinerals.findMostGeode(
+            blueprint,
+            Minerals(1, 0, 0, 0),
+            Minerals(0, 0, 0, 0),
+            Minerals(0, 0, 0, 0),
+            1
+        )
+        assertEquals(24, quality)
+    }
+
+    @Test
+    fun `should find the sum of blueprint qualities`() {
+        val blueprints = testInput.split("\n").map { NotEnoughMinerals.parseLine(it) }
+        val quality = NotEnoughMinerals.findSumOfBlueprintQualities(blueprints)
+        assertEquals(33, quality)
+    }
+
+    @Test
+    fun `should solve day 19 part 1`() {
+        val quality = NotEnoughMinerals.findSumOfBlueprintQualities(input)
+        assertEquals(2301, quality)
+    }
+
+    companion object {
+        private val testInput = """
+            Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+            Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+        """.trimIndent()
+
+        private val input by lazy { NotEnoughMinerals.readBlueprints() }
+    }
+}
